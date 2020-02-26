8e7629604e1ab6473520f9f2eaececddff4d6542
Raik <raik.becker@posteo.de>
2020-02-26 21:01:17 +0100

    Added examples

---
+++ examples/example_dispatch_one_day.py
@@ -0,0 +1,37 @@
+'''
+Example with one day of data.
+'''
+from gerd import models
+import pandas as pd
+
+# %% Read CSV input files
+# Generators
+input_gens = pd.read_csv('input_generators_kd.csv', index_col='name')
+# Storages
+input_stos = pd.read_csv('input_pump_storages.csv', index_col='name')
+# Load
+input_load = pd.read_csv(
+    'input_load_1d.csv', index_col='time', parse_dates=True)
+# Variable costs
+input_var_costs = pd.read_csv(
+    'input_var_costs_1d.csv', index_col='time', parse_dates=True)
+
+input_data = {'generators':input_gens,
+              'storages': input_stos,
+              'load': input_load,
+              'var_costs':input_var_costs}
+
+# %% Optimize without pump storage
+my_model = models.Dispatch(input_data, model_storages=False)
+my_model.optimize()
+# Look at the results
+my_model.solution['prices'].plot()
+my_model.solution['prod'].plot(kind='bar', stacked=True)
+
+# %% Optimize with pump storage
+my_model = models.Dispatch(input_data)
+my_model.optimize()
+# Look at the results
+my_model.solution['prices'].plot()
+my_model.solution['prod'].plot(kind='bar', stacked=True)
+my_model.solution['storage_cap'].plot()
