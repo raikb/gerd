8610a1aed91e960dd7cd7cd3f26055aa7a397766
Raik <raik.becker@posteo.de>
2020-02-26 21:38:48 +0100

    Update __init__.py

--- gerd/__init__.py
+++ gerd/__init__.py
@@ -3,5 +3,7 @@
     var: variable'''
 import logging
 import logging.config
+from os import path
 
-logging.config.fileConfig('logging.conf')
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
+logging.config.fileConfig(log_file_path)
