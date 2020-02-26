7db0cd0302233b9844de50e41f692aac85cb6dc9
Raik <raik.becker@posteo.de>
2020-02-26 21:38:18 +0100

    Update example_dispatch_one_day.py

--- examples/example_dispatch_one_day.py
+++ examples/example_dispatch_one_day.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # %% Read CSV input files
 # Generators
-input_gens = pd.read_csv('input_generators_kd.csv', index_col='name')
+input_gens = pd.read_csv('input_generators.csv', index_col='name')
 # Storages
 input_stos = pd.read_csv('input_pump_storages.csv', index_col='name')
 # Load
@@ -16,10 +16,10 @@ input_load = pd.read_csv(
 input_var_costs = pd.read_csv(
     'input_var_costs_1d.csv', index_col='time', parse_dates=True)
 
-input_data = {'generators':input_gens,
+input_data = {'generators': input_gens,
               'storages': input_stos,
               'load': input_load,
-              'var_costs':input_var_costs}
+              'var_costs': input_var_costs}
 
 # %% Optimize without pump storage
 my_model = models.Dispatch(input_data, model_storages=False)
